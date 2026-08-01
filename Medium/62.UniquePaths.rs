6e2cb5f84c542ab5408eae8d2935abba1d4ad0f2
Yoxi7 <justsudouser@tutamail.com>
2026-08-02 03:01:48 +0400

    62.UniquePaths.rs

---
+++ Medium/62.UniquePaths.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        let m = m as usize;
+        let n = n as usize;
+        let mut dp = vec![1; n];
+        for i in 1..m {
+            for j in 1..n {
+                dp[j] += dp[j-1];
+            }
+        }
+        dp[n-1] as i32
+    }
+}
